0572708228e7c1f4b9499b78ad93c2a5daeae39b
hmatheus00 <hmatheus00@outlook.com>
2021-08-10 15:03:59 -0300

    Adicao da logica das comparacoes das escolhas

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,42 @@
 
 const opcoes = ['pedra', 'papel', 'tesoura'];
+
+let pontuacaoUser = 0;
+let pontuacaoMaquina = 0;
+
+const pedra = document.getElementById('pedra');
+
+function comparar(button){
+    let posicao = parseInt(Math.random()*3);
+    let maquina = opcoes[posicao];
+    let opc = button.value;
+
+    console.log(opc);
+    console.log(maquina);
+
+    switch(opc){
+        case 'pedra':
+            if(maquina === 'tesoura'){
+                pontuacaoUser++;
+            }else if(maquina === 'papel'){
+                pontuacaoMaquina++;
+            }
+            break;
+        case 'papel':
+            if(maquina === 'pedra'){
+                pontuacaoUser++;
+            }else if(maquina === 'tesoura'){
+                pontuacaoMaquina++;
+            }
+            break;
+        case 'tesoura':
+            if(maquina === 'papel'){
+                pontuacaoUser++;
+            }else if(maquina === 'pedra'){
+                pontuacaoMaquina++;
+            }
+            break;
+    }
+    console.log('Pontuacao User - ' + pontuacaoUser);
+    console.log('Pontuacao Máquina - ' + pontuacaoMaquina);
+}
